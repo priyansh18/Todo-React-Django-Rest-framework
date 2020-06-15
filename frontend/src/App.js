f6f6909dccd77c8add116d799d8f8505d7aaa693
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-15 22:02:35 +0530

    :happy: Fetching data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,13 +18,17 @@ class App extends Component {
   fetchTask = () => {
     console.log("Fetching");
 
-    fetch("http://127.0.0.1:8000/api/task-list/").then((response) =>
-      response.json()
-    ).then(data=>{
-      console.log("Data:",data)
-    })
+    fetch("http://127.0.0.1:8000/api/task-list/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data:", data);
+        this.setState({
+          todoList: data,
+        });
+      });
   };
   render() {
+    var tasks = this.state.todoList;
     return (
       <div className="container">
         <div id="task-container">
@@ -52,7 +56,21 @@ class App extends Component {
               </div>
             </form>
           </div>
-          <div className="list-wrapper"></div>
+          <div className="list-wrapper">
+            {tasks.map((task, index) => (
+              <div key={index} className="task-wrapper flex-wrapper">
+                <div style={{ flex: 7 }}>
+                  <span>{task.title}</span>
+                </div>
+                <div style={{ flex: 1 }}>
+                  <button className="btn btn-sm btn-outline-info edit">Edit</button>
+                </div>
+                <div>
+                  <button className="btn btn-sm btn-outline-dark delete">-</button> 
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
